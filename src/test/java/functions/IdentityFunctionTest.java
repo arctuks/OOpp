46d9d7796a30c3cf211556aa4cc996e712f36641
arctuks <163pony@gmail.com>
2025-09-24 17:10:52 +0400

    - Migrated project to Maven - Added IdentityFunction with Unit tests(X)

---
+++ src/test/java/functions/IdentityFunctionTest.java
@@ -0,0 +1,16 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdentityFunctionTest {
+
+    @Test
+    void apply() {
+        MathFunction identity = new IdentityFunction();
+
+        for (double i = 1; i < 2; i+=0.26) {
+            assertEquals(i, identity.apply(i));
+        }
+    }
+}
