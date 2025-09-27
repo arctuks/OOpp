54a38ab1d26801d9ad446dc9b367d4bfe6e3839a
arctuks <163pony@gmail.com>
2025-09-27 14:24:36 +0400

    - Added interface TabulatedFunction (X)

---
+++ src/main/java/functions/TabulatedFunction.java
@@ -0,0 +1,17 @@
+package functions;
+
+public interface TabulatedFunction extends MathFunction {
+
+    int getCount();                       // Получение количества табулированных значений
+
+    double getX(int index);               // Получение значения аргумента x по номеру индекса
+    double getY(int index);               // Получение значения y по номеру индекса
+
+    void setY(int index, double value);   // Присвоить значение y по номеру индекса
+
+    int indexOfX(double x);               // Получение индекса х
+    int indexOfY(double y);               // Получание индекса у
+
+    double leftBound();                   // Получение самого левого х
+    double rightBound();                  // Получение самого правого х
+}
