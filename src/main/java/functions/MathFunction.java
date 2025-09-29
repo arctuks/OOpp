806ee5d6b98dd05ac85a619a3c2a861c66dc93b2
vika <viktory.savok@gmail.com>
2025-09-29 20:13:55 +0400

    Added andThen + test. ( Y 3.0)

--- src/main/java/functions/MathFunction.java
+++ src/main/java/functions/MathFunction.java
@@ -2,4 +2,10 @@ package functions;
 
 public interface MathFunction {
     double apply(double x);
+
+    default CompositeFunction andThen(MathFunction afterFunction) {
+        CompositeFunction object = new CompositeFunction(this, afterFunction);
+        return object;
+    }
 }
+
---
+++ src/test/java/functions/MathFunctionTest.java
@@ -0,0 +1,26 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathFunctionTest {
+
+    @Test
+    void andThen() {
+        MathFunction sin = new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return Math.sin(x);
+            }
+        };
+        SqrFunction xSqr = new SqrFunction();
+
+        MathFunction f1 = xSqr.andThen(xSqr);
+        assertEquals(16, f1.apply(2));
+
+        MathFunction f2 = sin.andThen(xSqr);
+        assertEquals(0.25, f2.apply(Math.PI /4));
+
+    }
+}
