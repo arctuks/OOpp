e65ef3d3b2e2f00b2989ddee67e4ef330c46f0e4
vika <viktory.savok@gmail.com>
2025-10-07 18:41:24 +0400

    update сomposite tubulated

---
+++ src/test/java/functions/CompositeTabulatedFunctionTest.java
@@ -0,0 +1,45 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CompositeTabulatedFunctionsTest {
+
+    @Test
+    void arrayTabulatedAndSqrFunction() {
+        ArrayTabulatedFunction f = new ArrayTabulatedFunction(new SqrFunction(), 0.0, 2.0, 3);
+        SqrFunction g = new SqrFunction();
+        MathFunction h = f.andThen(g);
+
+        assertEquals(1.0, h.apply(1.0), 1e-10);
+        assertEquals(16.0, h.apply(2.0), 1e-10);
+        assertEquals(6.25, h.apply(1.5), 1e-10);
+    }
+
+    @Test
+    void linkedListTabulatedAndIdentity() {
+        double[] x = {1.0, 2.0, 3.0};
+        double[] y = {10.0, 20.0, 30.0};
+        LinkedTabulatedFunction f = new LinkedTabulatedFunction(x, y);
+        IdentityFunction id = new IdentityFunction();
+        MathFunction h = f.andThen(id);
+
+        assertEquals(20.0, h.apply(2.0), 1e-10);
+        assertEquals(15.0, h.apply(1.5), 1e-10);
+    }
+
+    @Test
+    void arrayTabulatedAndLinkedListTabulated() {
+        ArrayTabulatedFunction f = new ArrayTabulatedFunction(new SqrFunction(), 0.0, 2.0, 3);
+        LinkedTabulatedFunction g = new LinkedTabulatedFunction(
+                x -> x * 2,
+                0.0, 4.0, 3
+        );
+        MathFunction h = f.andThen(g);
+
+        assertEquals(0.0, h.apply(0.0), 1e-10);
+        assertEquals(2.0, h.apply(1.0), 1e-10);
+        assertEquals(8.0, h.apply(2.0), 1e-10);
+        assertEquals(5.0, h.apply(1.5), 1e-10);
+    }
+}
