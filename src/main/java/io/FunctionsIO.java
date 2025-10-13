302fb3ed70eced4a4755bb1cae925ca8f0151bdd
arctuks <163pony@gmail.com>
2025-10-13 21:37:48 +0400

    (X) - Added method for deserialization

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -34,4 +34,9 @@ public final class FunctionsIO {
 
         return factory.create(xValues, yValues);
     }
+
+    public static TabulatedFunction deserialize(BufferedInputStream stream) throws IOException, ClassNotFoundException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(stream);
+        return (TabulatedFunction) objectInputStream.readObject();
+    }
 }
