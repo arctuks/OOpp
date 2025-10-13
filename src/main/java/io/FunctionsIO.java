c5f81acc4d5682509fae6b96fad261ed385e1094
arctuks <163pony@gmail.com>
2025-10-13 21:10:15 +0400

    (X) - Added method for reading Tabulated Function from buffered input stream

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -2,10 +2,11 @@ package io;
 
 import functions.TabulatedFunction;
 import functions.Point;
+import functions.factory.TabulatedFunctionFactory;
 import java.io.*;
 
 public final class FunctionsIO {
-    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
         DataOutputStream os = new DataOutputStream(outputStream);
         os.writeInt(function.getCount());
 
@@ -15,5 +16,21 @@ public final class FunctionsIO {
         }
 
         os.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) throws IOException {
+        DataInputStream is = new DataInputStream(inputStream);
+
+        int size = is.readInt();
+
+        double[] xValues = new double[size];
+        double[] yValues = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            xValues[i] = is.readDouble();
+            yValues[i] = is.readDouble();
+        }
+
+        return factory.create(xValues, yValues);
     }
 }
