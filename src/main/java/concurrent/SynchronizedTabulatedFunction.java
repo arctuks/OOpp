a076b079ef6351742c8c30357ac8bf8bb11581f4
arctuks <163pony@gmail.com>
2025-10-19 19:06:03 +0400

    (X) - Added SynchronizedTabulatedFunction with iterator method and tests

---
+++ src/main/java/concurrent/SynchronizedTabulatedFunction.java
@@ -0,0 +1,119 @@
+package concurrent;
+
+import functions.Point;
+import functions.TabulatedFunction;
+import operations.TabulatedFunctionOperationService;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SynchronizedTabulatedFunction implements TabulatedFunction {
+    private TabulatedFunction function;
+    private final Object lock;
+
+    public SynchronizedTabulatedFunction(TabulatedFunction function) {
+        this.function = function;
+        lock = this;
+    }
+
+    @Override
+    public int getCount() {
+        int count;
+        synchronized (lock) {
+            count = function.getCount();
+        }
+        return count;
+    }
+
+    @Override
+    public double getX(int index) {
+        double x;
+        synchronized (lock) {
+            x = function.getX(index);
+        }
+        return x;
+    }
+
+    @Override
+    public double getY(int index) {
+        double y;
+        synchronized (lock) {
+            y = function.getY(index);
+        }
+        return y;
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        synchronized (lock) {
+            function.setY(index, value);
+        }
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        int index;
+        synchronized (lock) {
+            index = function.indexOfX(x);
+        }
+        return index;
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        int index;
+        synchronized (lock) {
+            index = function.indexOfY(y);
+        }
+        return index;
+    }
+
+    @Override
+    public double leftBound() {
+        double left;
+        synchronized (lock) {
+            left = function.leftBound();
+        }
+        return left;
+    }
+
+    @Override
+    public double rightBound() {
+        double right;
+        synchronized (lock) {
+            right = function.rightBound();
+        }
+        return right;
+    }
+
+    @Override
+    public double apply(double x) {
+        double result;
+        synchronized (lock) {
+            result = function.apply(x);
+        }
+        return result;
+    }
+    @Override
+    public Iterator<Point> iterator() {
+        Point[] points = TabulatedFunctionOperationService.asPoints(function);
+
+        return new Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < points.length;
+            }
+
+            @Override
+            public Point next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                return points[index++];
+            }
+        };
+    }
+}
---
+++ src/test/java/concurrent/SynchronizedTabulatedFunctionTest.java
@@ -0,0 +1,134 @@
+package concurrent;
+
+import functions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SynchronizedTabulatedFunctionTest {
+    TabulatedFunction function = new LinkedListTabulatedFunction(new SqrFunction(), 1, 10, 10);
+    SynchronizedTabulatedFunction synchronizedTabulatedFunction;
+    int count = function.getCount();
+
+    @Test
+    void getCount() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+        assertEquals(count, synchronizedTabulatedFunction.getCount());
+    }
+
+    @Test
+    void getX() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+
+        for (int i = 0; i < count; i++) {
+            assertEquals(function.getX(i), synchronizedTabulatedFunction.getX(i));
+        }
+    }
+
+    @Test
+    void getY() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+
+        for (int i = 0; i < count; i++) {
+            assertEquals(function.getY(i), synchronizedTabulatedFunction.getY(i));
+        }
+    }
+
+    @Test
+    void setY() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+
+        for (int i = 0; i < count; i++) {
+            synchronizedTabulatedFunction.setY(i, 100);
+            assertEquals(100, synchronizedTabulatedFunction.getY(i));
+        }
+    }
+
+    @Test
+    void indexOfX() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+
+        for (int i = 0; i < count; i++) {
+            assertEquals(i, synchronizedTabulatedFunction.indexOfX(function.getX(i)));
+        }
+    }
+
+    @Test
+    void indexOfY() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+
+        for (int i = 0; i < count; i++) {
+            assertEquals(i, synchronizedTabulatedFunction.indexOfY(function.getY(i)));
+        }
+    }
+
+    @Test
+    void leftBound() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+
+        assertEquals(function.leftBound(), synchronizedTabulatedFunction.leftBound());
+    }
+
+    @Test
+    void rightBound() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+
+        assertEquals(function.rightBound(), synchronizedTabulatedFunction.rightBound());
+    }
+
+    @Test
+    void apply() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+
+        assertEquals(72.5, synchronizedTabulatedFunction.apply(8.5));
+        assertEquals(119, synchronizedTabulatedFunction.apply(11));
+    }
+
+    @Test
+    void iteratorInWhile() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+
+        Iterator<Point> iterator = synchronizedTabulatedFunction.iterator();
+        Point point;
+        int position = 0;
+
+        while (iterator.hasNext()) {
+            point = iterator.next();
+            assertEquals(synchronizedTabulatedFunction.getY(position), point.y);
+            assertEquals(synchronizedTabulatedFunction.getX(position), point.x);
+            position++;
+        }
+
+        assertEquals(10, position);
+    }
+
+    @Test
+    void iteratorInForEach() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+        int position = 0;
+
+        for (Point point : synchronizedTabulatedFunction) {
+            assertEquals(synchronizedTabulatedFunction.getY(position), point.y);
+            assertEquals(synchronizedTabulatedFunction.getX(position), point.x);
+            position++;
+        }
+
+        assertEquals(10, position);
+    }
+
+    @Test
+    void iteratorException() {
+        synchronizedTabulatedFunction = new SynchronizedTabulatedFunction(function);
+        Iterator<Point> iterator = synchronizedTabulatedFunction.iterator();
+
+        while (iterator.hasNext()) {
+            iterator.next();
+        }
+
+        assertThrows(NoSuchElementException.class, iterator::next);
+        assertFalse(iterator.hasNext());
+    }
+}
