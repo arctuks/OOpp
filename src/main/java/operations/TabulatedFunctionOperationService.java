a4dd7b76a124cad3a21b59ce5da18b91d79fb07b
vika <viktory.savok@gmail.com>
2025-10-21 03:45:32 +0500

    added TabulatedFunctionOperationService + Point[] asPoints Y
    Signed-off-by: vika <viktory.savok@gmail.com>

--- src/main/java/operations/TabulatedFunctionOperationService.java
+++ src/main/java/operations/TabulatedFunctionOperationService.java
@@ -1,26 +1,29 @@
 package operations;
 
+
+import functions.Point;
 import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
+
+
+
 public class TabulatedFunctionOperationService {
-    public TabulatedFunction multiply(TabulatedFunction a, TabulatedFunction b) {
-        return doOperation(a, b, new BiOperation() {
-            @Override
-            public double apply(double u, double v) {
-                return u * v;
-            }
-        });
+    public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
+        // Инициализация массива для хранения точек
+        Point[] points = new Point[tabulatedFunction.getCount()];
+
+        // Индекс для записи точек в массив
+        int i = 0;
+
+        // Цикл for-each для итерирования по табулированной функции
+        for (Point point : tabulatedFunction) {
+            points[i] = point;
+            i++; // Инкремент индекса для следующей точки
+        }
+
+        return points;
     }
 
-    // Метод деления
-    public TabulatedFunction divide(TabulatedFunction a, TabulatedFunction b) {
-        return doOperation(a, b, new BiOperation() {
-            @Override
-            public double apply(double u, double v) {
-                if (v == 0.0) {
-                    throw new IllegalArgumentException("Divide by Zero");
-                }
-                return u / v;
-            }
-        });
-    }
+
 }
