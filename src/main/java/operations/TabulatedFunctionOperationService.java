c355cb177d8de5430a39e30bdcd8fc4fdcb84d4f
vika <viktory.savok@gmail.com>
2025-10-21 04:08:17 +0500

    added InconsistentFunctionsException + all in TabulatedFunctionOperationService (Y)
    Signed-off-by: vika <viktory.savok@gmail.com>

--- src/main/java/operations/TabulatedFunctionOperationService.java
+++ src/main/java/operations/TabulatedFunctionOperationService.java
@@ -1,11 +1,13 @@
 package operations;
 
+import java.util.Iterator;
 
 import functions.Point;
-import functions.TabulatedFunction;
+import exceptions.InconsistentFunctionsException;
 import functions.factory.ArrayTabulatedFunctionFactory;
 import functions.factory.TabulatedFunctionFactory;
 
+import functions.TabulatedFunction;
 
 public class TabulatedFunctionOperationService {
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
@@ -23,6 +25,73 @@ public class TabulatedFunctionOperationService {
 
         return points;
     }
+    private TabulatedFunctionFactory factory;
 
+    // Конструктор с фабрикой
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    // Конструктор по умолчанию, использующий ArrayTabulatedFunctionFactory
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    // Геттер для фабрики
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    // Сеттер для фабрики
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    // Вложенный интерфейс для бинарных операций
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    // Метод для выполнения бинарной операции над двумя функциями
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        // Проверка на совместимость функций
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException("У функций разное количество точек");
+        }
+
+        Point[] pointsA = asPoints(a);
+        Point[] pointsB = asPoints(b);
+
+        double[] xValues = new double[a.getCount()];
+        double[] yValues = new double[a.getCount()];
+
+        for (int i = 0; i < a.getCount(); i++) {
+            if (pointsA[i].x != pointsB[i].x) {
+                throw new InconsistentFunctionsException("У функций разные значения X");
+            }
+            xValues[i] = pointsA[i].x;
+            yValues[i] = operation.apply(pointsA[i].y, pointsB[i].y); // Применение операции
+        }
+
+        return factory.create(xValues, yValues); // Создание новой функции с использованием фабрики
+    }
+
+    // Метод для сложения двух функций
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+
+    // Метод для вычитания двух функций
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
+    }
+
+    public TabulatedFunction multiply(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u * v);
+    }
+
+    public TabulatedFunction divide(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u / v);
+    }
 
 }
