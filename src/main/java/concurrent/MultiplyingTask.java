ca4f3bcc3a1c9dbd611cb6221404bce8530d4f96
vika <viktory.savok@gmail.com>
2025-10-21 06:57:51 +0500

    4 lab   added MultiplyingTask
    Signed-off-by: vika <viktory.savok@gmail.com>

---
+++ src/main/java/concurrent/MultiplyingTask.java
@@ -0,0 +1,23 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+    private final TabulatedFunction function;
+
+    public MultiplyingTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        // Увеличиваем значение y в два раза для каждой точки
+        for (int i = 0; i < function.getCount(); i++) {
+            synchronized (function) {
+                double currentY = function.getY(i);
+                function.setY(i, currentY * 2);
+            }
+        }
+        System.out.println("Thread " + Thread.currentThread().getName() + " completed the task");
+    }
+}
